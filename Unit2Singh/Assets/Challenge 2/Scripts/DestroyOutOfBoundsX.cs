dbf7c001dd088287b07a31134778e10736a43cf1
Singh <730222@student.dusd.net>
2023-03-13 11:41:25 -0700

    Almost finished

--- Unit2Singh/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
+++ Unit2Singh/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -1,28 +1,24 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyOutOfBounds : MonoBehaviour
+public class DestroyOutOfBoundsX : MonoBehaviour
 {
-    private float topBound = 30;
-    private float lowerBound = -10;
+    private float leftLimit = -30;
+    private float bottomLimit = -5;
     private float sideBound = 30;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        //if an object goes past the players view in the game, remove that object
-        if (transform.position.z > topBound)
+        // Destroy dogs if x position less than left limit
+        if (transform.position.x < leftLimit)
         {
             Destroy(gameObject);
-        }   else if (transform.position.z < lowerBound)
+        } 
+        // Destroy balls if y position is less than bottomLimit
+        else if (transform.position.y < bottomLimit)
         {
-            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
         else if (transform.position.x > sideBound)
--- Unit2Singh/Assets/Scripts/SpawnManager.cs
+++ Unit2Singh/Assets/Scripts/SpawnManager.cs
@@ -9,6 +9,10 @@ public class SpawnManager : MonoBehaviour
     private float spawnPosZ = 20;
     private float StartDelay = 2;
     private float SpawnInterval = 1.5f;
+    
+    public float sideSpawnMinZ;
+    public float sideSpawnMaxZ;
+    public float sideSpawnX;
 
     // Start is called before the first frame update
     void Start()
@@ -29,4 +33,27 @@ public class SpawnManager : MonoBehaviour
 
         Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
+
+    void SpawnLeftAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(-sideSpawnX, 0, Random.Range(sideSpawnMinZ,
+        sideSpawnMaxZ));
+        Instantiate(animalPrefabs[animalIndex], spawnPos,
+        animalPrefabs[animalIndex].transform.rotation);
+    }
+
+
+
+    void SpawnRightAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(sideSpawnX, 0, Random.Range(sideSpawnMinZ,
+        sideSpawnMaxZ));
+        Vector3 rotation = new Vector3(0, -90, 0);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, Quaternion.Euler(rotation));
+    }
+
+
+
 }
